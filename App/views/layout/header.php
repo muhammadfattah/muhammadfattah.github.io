d535420c8a08372103c3b69495c06afc948a7940
muhammadfattah <muhammadfattah619@gmail.com>
2021-03-07 15:16:49 +0700

    nambahin file bootstrap

--- App/views/layout/header.php
+++ App/views/layout/header.php
@@ -3,6 +3,8 @@
 
 <head>
     <title><?= ucfirst($page); ?> | My Portpolio</title>
+     <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css/'); ?>">
 </head>
 
