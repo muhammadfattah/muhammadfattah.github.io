f815568f1bc443ed8b92586c934ef7c617bb5b10
muhammadfattah <muhammadfattah619@gmail.com>
2021-03-07 15:31:56 +0700

    first commit

--- App/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$base_url = 'https://muhammadfattah.github.io/';
-
-function base_url($url = ''){
-    global $base_url;
-  return trim($base_url,'/').'/'.trim($url,'/');
-}
-?>
--- App/views/error/404.php
+++
@@ -1,2 +0,0 @@
-<h1>Pages Not Found</h1>
-<a href="?page=home">Back to Home</a>
