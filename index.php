c82e7208019758f21db58c1489600672022984a2
muhammadfattah <muhammadfattah619@gmail.com>
2021-03-07 15:12:51 +0700

    coba

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
-require 'App/config/config.php';
-if (isset($_GET['page'])) {
-  $page = $_GET['page'];
-} else {
-  $page = 'home';
-}
-$path = 'App/views/';
+// require 'App/config/config.php';
+// if (isset($_GET['page'])) {
+//   $page = $_GET['page'];
+// } else {
+//   $page = 'home';
+// }
+// $path = 'App/views/';
 
-if (is_file($path.'pages/' . $page . '.php')) {
-  require $path.'layout/header.php';
-  require $path.'pages/' . $page . '.php';
-  require $path.'layout/footer.php';
-} else {
-  require $path.'error/404.php';
-}
+// if (is_file($path.'pages/' . $page . '.php')) {
+//   require $path.'layout/header.php';
+//   require $path.'pages/' . $page . '.php';
+//   require $path.'layout/footer.php';
+// } else {
+//   require $path.'error/404.php';
+// }
+?>
+<h1>Hello World</h1>
